533ed77192c31700929b8ae60aa90e932b853d37
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-04-10 12:44:19 +0800

    fix: only accept json

--- src/fetch.ts
+++ src/fetch.ts
@@ -2,7 +2,10 @@ import type { H3Event } from 'h3'
 
 export async function getVersions(evt: H3Event, pkg: string, setStatus = true) {
   const response = await fetch(`https://jsr.io/${pkg}/meta.json`, {
-    headers: { 'User-Agent': 'https://github.com/sxzz/jsr-api' },
+    headers: {
+      'User-Agent': 'https://github.com/sxzz/jsr-api',
+      Accept: 'application/json',
+    },
   })
   setStatus && setResponseStatus(evt, response.status)
   return response
